69f875c3c7bcb8390fc02e4ab796322e7f161315
koffskeyj <koffskeyj@gmail.com>
2016-07-07 11:47:42 -0400

    started app

--- hello_heroku/urls.py
+++ hello_heroku/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.conf.urls import url
 from django.contrib import admin
 
+
+
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^$', IndexView.as_view(), name="index_view")
 ]
